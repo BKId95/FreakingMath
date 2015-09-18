7a2fb820af05e595901ef897ff239258ecf93bd2
BKid95 <thachpn95@gmail.com>
2015-09-19 02:42:36 +0700

    demo2

---
+++ Programs/AndroidProjects/FreakingMath2/app/src/main/java/com/example/pnt/freakingmath2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pnt.freakingmath2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import  android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        FragmentPlay fragment = new FragmentPlay();
+        ft.add(R.id.container,fragment);
+        ft.commit();
+    }
+
+}
